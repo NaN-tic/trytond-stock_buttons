ce8b986aad99f4f237e9b891cac626ec34142ef9
Raimon Esteve <raimonesteve@gmail.com>
2020-11-28 17:07:29 +0100

    Add extras to pass extra depends tests

--- tests/test_stock_buttons.py
+++ tests/test_stock_buttons.py
@@ -4,12 +4,25 @@
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
+import trytond.tools as tools
+import os
+
+OPJ = os.path.join
+MODULES_PATH = os.path.abspath(__file__)
+# MODULES_PATH = os.path.abspath('../..')
 
 
 class StockButtonsTestCase(ModuleTestCase):
     'Test Stock Buttons module'
     module = 'stock_buttons'
-
+    extras = []
+    for extra_depend in ('product_manufacturer', 'stock_split',
+            'stock_external_party', 'stock_lot', 'stock_valued'):
+        try:
+            tools.file_open(os.path.join(extra_depend, 'tryton.cfg'))
+            extras.append(extra_depend)
+        except IOError:
+            pass
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
