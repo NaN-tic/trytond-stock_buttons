02821372b8a7862f2146d8e10e18236ff445b7c7
Raimon Esteve <raimonesteve@gmail.com>
2025-05-20 12:44:43 +0200

    FIX Removing deprecated args from setup

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
 from setuptools import setup
 import re
 import os
@@ -7,11 +8,8 @@ import io
 from configparser import ConfigParser
 
 MODULE = 'stock_buttons'
-PREFIX = 'trytonspain'
-MODULE2PREFIX = {
-    'stock_valued': 'nan-tic',
-    'product_manufacturer': 'nan-tic',
-    }
+PREFIX = 'nantic'
+MODULE2PREFIX = {}
 
 
 def read(fname):
@@ -42,39 +40,23 @@ major_version = int(major_version)
 minor_version = int(minor_version)
 
 requires = []
-
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         prefix = MODULE2PREFIX.get(dep, 'trytond')
         requires.append(get_require_version('%s_%s' % (prefix, dep)))
 requires.append(get_require_version('trytond'))
-requires += [get_require_version('trytond_stock')]
 
 tests_require = [
     get_require_version('proteus'),
-    
-    ]
+]
 
 series = '%s.%s' % (major_version, minor_version)
 if minor_version % 2:
-    branch = 'master'
+    branch = 'default'
 else:
     branch = series
 
-dependency_links = [
-   ('git+https://github.com/trytonspain/'
-       'trytond-stock_valued@%(branch)s'
-       '#egg=trytonspain-stock_valued%(series)s'%{
-               'branch': branch, 
-               'series': series,}),
-
-
-   ('git+https://github.com/nanticzz/'
-       'trytond-product_manufacturer@%(branch)s'
-       '#egg=trytonzz-product_manufacturer%(series)s'%{
-               'branch': branch, 
-               'series': series,}),
-]
+dependency_links = []
 
 if minor_version % 2:
     # Add development index for testing with proteus
@@ -84,9 +66,10 @@ setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
     description='',
     long_description=read('README'),
-    author='trytonspain',
+    author='NaN·tic',
+    author_email='info@nan-tic.com',
     url='http://www.nan-tic.com/',
-    download_url='https://github.com:trytonspain/trytond-stock_buttons',
+    download_url="https://bitbucket.org/nantic/trytond-%s" % MODULE,
     package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
         'trytond.modules.%s' % MODULE,
@@ -94,7 +77,7 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         ],
     package_data={
         'trytond.modules.%s' % MODULE: (info.get('xml', [])
-            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst', 'view/*.xml']),
+            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -104,12 +87,22 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
         'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: Bulgarian',
         'Natural Language :: Catalan',
+        'Natural Language :: Czech',
+        'Natural Language :: Dutch',
         'Natural Language :: English',
+        'Natural Language :: French',
+        'Natural Language :: German',
+        'Natural Language :: Russian',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Office/Business',
         ],
     license='GPL-3',
@@ -123,5 +116,4 @@ setup(name='%s_%s' % (PREFIX, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
-
     )
